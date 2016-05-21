a855a795a5c86aaa2bae6d8ea55633d65c416518
bloed <josuearrietasalas@gmail.com>
2016-05-21 17:03:20 -0600

    estoy a pura consola jaja

--- SistemaDeAmortizacion/src/Datos/Bitacora/BitacoraCSV.java
+++ SistemaDeAmortizacion/src/Datos/Bitacora/BitacoraCSV.java
@@ -1,8 +1,3 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
 package Datos.Bitacora;
 
 import DTOs.DTOModeloVista;
@@ -17,10 +12,6 @@ import java.io.StringWriter;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 
-/**
- *
- * @author fm010
- */
 public class BitacoraCSV extends Bitacora{
   private StringWriter escritor;
 
